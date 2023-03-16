d2024f664c383288e6d519e9e7ac79b53586fa54
jotapemattos <jpmrc49@gmail.com>
2023-03-16 15:05:50 -0300

    responsiveness on movie page finished

--- src/pages/MoviePage.tsx
+++ src/pages/MoviePage.tsx
@@ -66,11 +66,15 @@ const MoviePage = () => {
 }) 
 
 
-
+const [hover, setHover] = useState(false)
 const [recomendedMovies, setRecomendedMovies] = useState(Array<MoviesDataType>)
 const [cast, setCast] = useState<CastTypes[]>([{name: ''}, {name: ''}, {name: ''}])
 const [crew, setCrew] = useState<CrewTypes[]>([{name:''}])
 const params = useParams()
+
+  useEffect(()=> {
+    window.scrollTo(0, 0)
+  }, [data.title])
 
   useEffect(()=>{
     axios.get(`https://api.themoviedb.org/3/${params.description}/${params.id}?api_key=c0ab09e8c5c085013b036d4c56c1d944&language=en-US`)
@@ -96,31 +100,30 @@ const params = useParams()
     })
   }, [data])
 
-  console.log(data.id)
 
   return (
 
     <div
-    className="flex flex-col items-center gap-20 mt-10 min-h-full w-full text-white fixed">
+    className="flex flex-col items-center gap-14 xl:gap-28 mt-10 h-auto w-full text-white">
         <motion.div 
-            className="flex text-center justify-center min-h-full w-full "
+            className="flex text-center justify-center min-h-full w-full"
             initial={{ x: '-100vw' }}
             animate={{ x: 0 }}
             transition={{ duration: 0.7}}
         >
             {data.title && (
-                <h1 className="font-extrabold text-6xl font-">{data.title}</h1>
+                <h1 className="font-extrabold text-3xl md:text-4xl xl:text-6xl">{data.title}</h1>
             )}
             {data.name && (
-                <h1 className="font-extrabold text-6xl font-">{data.name}</h1>
+                <h1 className="font-extrabold text-3xl md:text-4xl lg:text-6xl">{data.name}</h1>
             )}
         </motion.div>
-        <div className="flex items-start justify-around">
-            <div className=" min-w-1/3 w-1/5 mb-10 text-center flex flex-col gap-5">
+        <div className="flex flex-col gap-5  md:flex-row items-center md:items-start justify-between w-full md:w-5/6 lg:w-4/5 xl:w-2/3 h-96 md:h-80 lg:h-96">
+            <div className="h-full w-72 mb-10 text-center flex flex-col gap-5">
                 <motion.img 
                     src= {imageURL + data.poster_path} 
                     alt="" 
-                    className="h-full w-full rounded-xl"
+                    className="h-full md:h-5/6 lg:h-[333px] xl:h-full w-full"
                     initial={{ x: '-100vw' }}
                     animate={{ x: 0 }}
                     transition={{ duration: 0.7}}
@@ -131,10 +134,20 @@ const params = useParams()
                     transition={{ duration: 0.7 }}
                     className="flex gap-5 items-center justify-around box-border"
                 >
-                    {data.genres.map((genre)=>(
+                    {data.genres.map((genre, i)=> i < 3 && (
                         <p key={genre.id} className="text-center px-2">{genre.name}</p>
                     ))}
                 </motion.div>
+            </div>
+            <div className="h-full w-auto text-center flex-col gap-5 hidden md:flex">
+                <motion.img 
+                    src= {imageURL + data.backdrop_path} 
+                    alt="" 
+                    className="h-full w-full"
+                    initial={{ x: '-100vw' }}
+                    animate={{ x: 0 }}
+                    transition={{ duration: 0.7}}
+                />
                 <div className="flex justify-around ">
                     <div className="flex items-center justify-between gap-2">
                         <Star size={32} weight='fill' fill="yellow"/>
@@ -146,24 +159,14 @@ const params = useParams()
                     </div>
                 </div>
             </div>
-            <motion.div 
-                className="w-1/2 flex min-h-full flex-col gap-10 "
-                initial={{ opacity: 0, scale: 0.5 }}
-                animate={{ opacity: 1, scale: 1 }}
-                transition={{ duration: 0.7 }}
-            >
-                <div className="flex flex-col max-h-full gap-10 bg-gradient-to-br from-white/10 to-white/20 backdrop-opacity-50 shadow-xl p-4 rounded-xl">
-                    {data.tagline && (<p className="text-2xl italic text-bold font-handLetter" >{data.tagline}</p>)}
+        </div>
+        <div className="flex flex-col w-full md:w-5/6 lg:w-4/5 xl:w-2/3 gap-10 p-4 rounded-xl">
+                    {data.tagline && (<p className="text-3xl italic text-bold" >"{data.tagline}"</p>)}
                     {data.overview && (
                         <p className="text-lg">{data.overview}</p>
                     )}
-                    <div className="flex justify-between">
+                    <div className="flex flex-col md:flex-row justify-between gap-10">
                         {data.runtime && (<p className="text-lg">Duration: {data.runtime} min</p>)}
-                        {data.number_of_seasons && (<p className="text-lg">Seasons: {data.number_of_seasons}</p>)}
-                        {cast.length>2 && (<p className="text-lg">Actors: {cast[0].name}, {cast[1].name}, {cast[2].name}</p>)}
-                        {cast.length<3 && (<p className="text-lg">Actors: {cast[0].name}, {cast[1].name}</p>)}
-                    </div>
-                    <div className="flex justify-between">
                         {data.release_date && (
                             <div className="flex items-center gap-4">
                             <VideoCamera size={32} weight='fill' fill="#fa0505"/>
@@ -176,61 +179,99 @@ const params = useParams()
                                 <p className="text-lg">First live: {data.first_air_date.slice(0,4)}</p>
                             </div>
                         )}
-                        {crew.length > 0 && (<p className="text-lg">Directed by: {crew[0].name}</p>)}
+                        {data.number_of_seasons && (<p className="text-lg">Seasons: {data.number_of_seasons}</p>)}
+                        
                     </div>
-                    {typeof data.budget === 'number' && (
-                        <div>
-                        {(data.revenue - data.budget < 0)? (
-                            <div className="flex items-center justify-start gap-2">
-                                <TrendDown size={32} weight='fill' fill="red"/>
-                             <p className="text-xl">U$$ {(data.revenue - data.budget).toLocaleString('en-US')}</p>
-                            </div>
-                        ):(
-                            <div className="flex items-center justify-start gap-2">
-                                <p className="text-xl">U$$ {(data.revenue - data.budget).toLocaleString('en-US')}</p>
-                                <TrendUp size={32} weight='fill' fill="green"/>
-                            </div>
-                        )}
+                    <div className="flex flex-col justify-between gap-10">
+                        <div className="flex flex-col md:flex-row gap-10 justify-between">
+                            {crew.length > 0 && (<p className="text-lg">Directed by: {crew[0].name}</p>)}
+                            {typeof data.budget === 'number' && (
+                                <div className="hidden md:flex">
+                                    {(data.revenue - data.budget < 0)? (
+                                        <div className="flex items-center justify-start gap-2">
+                                            <TrendDown size={32} weight='fill' fill="red"/>
+                                            <p className="text-xl">U$$ {(data.revenue - data.budget).toLocaleString('en-US')}</p>
+                                        </div>
+                                    ):(
+                                        <div className="flex items-center justify-start gap-2">
+                                            <TrendUp size={32} weight='fill' fill="green"/>
+                                            <p className="text-xl">U$$ {(data.revenue - data.budget).toLocaleString('en-US')}</p>
+                                        </div>
+                                    )}
+                                </div>
+                            )}
+                        </div>
+                        {cast.length>2 && (<p className="text-lg">Actors: {cast[0].name}, {cast[1].name}, {cast[2].name}</p>)}
+                        {cast.length===1 && (<p className="text-lg">Actors: {cast[0].name}</p>)}
+                        {cast.length<3 && cast.length > 1 && (<p className="text-lg">Actors: {cast[0].name}, {cast[1].name}</p>)}
+                        {typeof data.budget === 'number' && (
+                                <div className="flex md:hidden">
+                                    {(data.revenue - data.budget < 0)? (
+                                        <div className="flex items-center justify-start gap-2">
+                                            <p className="text-xl">U$$ {(data.revenue - data.budget).toLocaleString('en-US')}</p>
+                                            <TrendDown size={32} weight='fill' fill="red"/>
+                                        </div>
+                                    ):(
+                                        <div className="flex items-center justify-start gap-2">
+                                            <p className="text-xl">U$$ {(data.revenue - data.budget).toLocaleString('en-US')}</p>
+                                            <TrendUp size={32} weight='fill' fill="green"/>
+                                        </div>
+                                    )}
+                                </div>
+                            )}
                     </div>
-                    )}
                 </div>
-                <div className="min-h-full flex flex-col gap-4">
-                    {recomendedMovies.length>0 && (<h1 className="text-2xl italic text-bold font-handLetter">More like this</h1>)}
-                    <Carousel
+                <div className="h-full w-full xl:w-3/4 flex flex-col gap-4 mb-10">
+                    {recomendedMovies.length>0 && (<h1 className="text-3xl w-full text-center text-bold">More like this</h1>)}
+                    {<Carousel
                         withIndicators
                         slideSize="8%"
                         slideGap="lg"
                         containScroll= "trimSnaps"
-                        /* breakpoints={[
-                        { maxWidth: 'md', slideSize: '50%' },
-                        { maxWidth: 'sm', slideSize: '100%', slideGap: 0 },
-                        ]} */
-                        previousControlIcon={(<div className='bg-white z-50 opacity-100 rounded-full hover:scale-125 duration-300'><CaretLeft size={35} weight='regular' color='#27272a'/></div>)}
-                        nextControlIcon={(<div className='bg-white z-50 opacity-100 rounded-full hover:scale-125 duration-300'><CaretRight size={35} weight='regular' color='#27272a'/></div>)}
+                        previousControlIcon={ hover && (
+                            <motion.div
+                              initial={{ opacity: 0}}
+                              animate={{ opacity: 1}}
+                              transition={{ duration: 1}}
+                            >
+                              <CaretLeft size={50} weight='regular' color='#ffffff'/>
+                            </motion.div>
+                          )}
+                          nextControlIcon={hover && (
+                            <motion.div
+                              initial={{ opacity: 0}}
+                              animate={{ opacity: 1}}
+                              transition={{ duration: 1}}
+                  
+                            >
+                              <CaretRight size={50} weight='regular' color='#ffffff'/>
+                            </motion.div>
+                          )}
                         align="start"  
-                        className="flex h-60 px-16 py-4"
+                        className="flex h-full px-4 lg:px-14 xl:px-16 py-4"
+                        onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)}
                     >
                         {recomendedMovies.map((movie)=>(
                             <Carousel.Slide
-                            className="h-10 min-w-1/8 bg-gradient-to-br from-white/10 to-white/20 backdrop-opacity-50 shadow-xl mx-2 px-2 pt-2 text-white font-extrabold rounded-xl cursor-pointer hover:scale-90 hover:z-2 hover:brightness-50 transition-all duration-300" 
+                            className="w-full xl:w-5/6 h-3/5 mt-6 flex gap-3 z-0 justify-center items-center md:px-4 pt-2" 
                             >
+                                <div className="h-96 w-[300px] md:w-[250px] shadow-xl bg-zinc-300/5 text-white font-extrabold hover:brightness-50 hover:scale-95 hover:z-2 cursor-pointer transition-all duration-500">
                                 <Link 
                                     key={movie.id}
                                     to={'/movie/' + movie.id}
                                 >
-                                    <img src= {imageURL + movie.poster_path} alt="movie-poster" className="h-4/5 w-full rounded-xl"/>
-                                    <div className="h-1/5 flex items-center justify-around">
+                                    <img src= {imageURL + movie.poster_path} alt="movie-poster" className="h-5/6 w-full"/>
+                                    <div className="h-1/5 flex items-center justify-center gap-2">
                                         <Star weight="fill" fill='yellow'/>
                                         <p>{(movie.vote_average).toFixed(1)}</p>
                                     </div>
                                 </Link>
+                                </div>
                             </Carousel.Slide>
                         ))}
-                    </Carousel>
+                    </Carousel>}
                 </div>
-            </motion.div>
         </div>
-    </div>
-  )
+    )
 }
 export default MoviePage
